b5b145c062bba20274a4a655f3b5d2332229e22c
Danny Piper <djpiper28@gmail.com>
2022-08-26 16:18:28 +0100

    Update RorkesDrift.lua

--- RorkesDrift.lua
+++ RorkesDrift.lua
@@ -309,6 +309,230 @@ function AddFeatures()
 	featuregen:AddFeatures();
 end
 ------------------------------------------------------------------------------
+function AssignStartingPlots:GenerateRegions(args)
+	print("Map Generation - Dividing the map in to Regions");
+	-- This is a customized version for West vs East.
+	-- This version is tailored for handling two-teams play.
+	local args = args or {};
+	local iW, iH = Map.GetGridSize();
+	local res = Map.GetCustomOption(13)
+	if res == 9 then
+		res = 1 + Map.Rand(3, "Random Resources Option - Lua");
+	end
+
+	local setback = Map.GetCustomOption(15)-1;
+
+	local setforward = Map.GetCustomOption(16)-1;
+
+	local setrange = setforward + setback;
+
+	print("Moveback: ", setback);
+
+	self.resource_setting = res; -- Each map script has to pass in parameter for Resource setting chosen by user.
+	self.method = 3; -- Flag the map as using a Rectangular division method.
+	
+	-- Determine number of civilizations and city states present in this game.
+	self.iNumCivs, self.iNumCityStates, self.player_ID_list, self.bTeamGame, self.teams_with_major_civs, self.number_civs_per_team = GetPlayerAndTeamInfo()
+	self.iNumCityStatesUnassigned = self.iNumCityStates;
+	print("-"); print("Civs:", self.iNumCivs); print("City States:", self.iNumCityStates);
+
+	-- Determine number of teams (of Major Civs only, not City States) present in this game.
+	iNumTeams = table.maxn(self.teams_with_major_civs);				-- GLOBAL
+	print("-"); print("Teams:", iNumTeams);
+
+	-- If four teams are present, use team-oriented handling of start points, one team in each corner.
+	if iNumTeams == 4 then
+		print("-"); print("Number of Teams present is four! Using custom team start placement for DefianceCorners."); print("-");
+		
+		-- ToDo: Correctly identify team IDs and how many Civs are on each team.
+		-- Also need to shuffle the teams so its random who starts on which half.
+		iNumDivisions = 2
+
+		-- Process the bottom left corner.
+		self.inhabited_WestX = 0 + setforward;
+		self.inhabited_SouthY = 0 + setforward;
+		self.inhabited_Width = (math.floor(iW / 2)) - setrange;
+		self.inhabited_Height = (math.floor(iH / 2)) - setrange;
+		-- Obtain "Start Placement Fertility" inside the rectangle.
+		-- Data returned is: fertility table, sum of all fertility, plot count.
+		local fert_table, fertCount, plotCount = self:MeasureStartPlacementFertilityInRectangle(self.inhabited_WestX, 
+		                                         self.inhabited_SouthY, self.inhabited_Width, self.inhabited_Height)
+		-- Assemble the Rectangle data table:
+		local rect_table = {self.inhabited_WestX, self.inhabited_SouthY, self.inhabited_Width, 
+		                    self.inhabited_Height, -1, fertCount, plotCount}; -- AreaID -1 means ignore area IDs.
+		-- Divide the rectangle.
+		self:DivideIntoRegions(iNumDivisions, fert_table, rect_table)
+
+		-- Process the bottom right corner.
+		self.inhabited_WestX = (math.floor(iW / 2)) + setback;
+		self.inhabited_SouthY = 0 + setforward;
+		self.inhabited_Width = (math.floor(iW / 2)) - setrange;
+		self.inhabited_Height = (math.floor(iH / 2)) - setrange;
+		-- Obtain "Start Placement Fertility" inside the rectangle.
+		-- Data returned is: fertility table, sum of all fertility, plot count.
+		local fert_table, fertCount, plotCount = self:MeasureStartPlacementFertilityInRectangle(self.inhabited_WestX, 
+		                                         self.inhabited_SouthY, self.inhabited_Width, self.inhabited_Height)
+		-- Assemble the Rectangle data table:
+		local rect_table = {self.inhabited_WestX, self.inhabited_SouthY, self.inhabited_Width, 
+		                    self.inhabited_Height, -1, fertCount, plotCount}; -- AreaID -1 means ignore area IDs.
+		-- Divide the rectangle.
+		self:DivideIntoRegions(iNumDivisions, fert_table, rect_table)
+		
+		-- Process the top left corner.
+		self.inhabited_WestX = 0 + setforward;
+		self.inhabited_SouthY = (math.floor(iH / 2)) + setback;
+		self.inhabited_Width = (math.floor(iW / 2)) - setrange;
+		self.inhabited_Height = (math.floor(iH / 2)) - setrange;
+		-- Obtain "Start Placement Fertility" inside the rectangle.
+		-- Data returned is: fertility table, sum of all fertility, plot count.
+		local fert_table, fertCount, plotCount = self:MeasureStartPlacementFertilityInRectangle(self.inhabited_WestX, 
+		                                         self.inhabited_SouthY, self.inhabited_Width, self.inhabited_Height)
+		-- Assemble the Rectangle data table:
+		local rect_table = {self.inhabited_WestX, self.inhabited_SouthY, self.inhabited_Width, 
+		                    self.inhabited_Height, -1, fertCount, plotCount}; -- AreaID -1 means ignore area IDs.
+		-- Divide the rectangle.
+		self:DivideIntoRegions(iNumDivisions, fert_table, rect_table)
+
+		-- Process the top right corner.
+		self.inhabited_WestX = (math.floor(iW / 2)) + setback;
+		self.inhabited_SouthY = (math.floor(iH / 2)) + setback;
+		self.inhabited_Width = (math.floor(iW / 2)) - setrange;
+		self.inhabited_Height = (math.floor(iH / 2)) - setrange;
+		-- Obtain "Start Placement Fertility" inside the rectangle.
+		-- Data returned is: fertility table, sum of all fertility, plot count.
+		local fert_table, fertCount, plotCount = self:MeasureStartPlacementFertilityInRectangle(self.inhabited_WestX, 
+		                                         self.inhabited_SouthY, self.inhabited_Width, self.inhabited_Height)
+		-- Assemble the Rectangle data table:
+		local rect_table = {self.inhabited_WestX, self.inhabited_SouthY, self.inhabited_Width, 
+		                    self.inhabited_Height, -1, fertCount, plotCount}; -- AreaID -1 means ignore area IDs.
+		-- Divide the rectangle.
+		self:DivideIntoRegions(iNumDivisions, fert_table, rect_table)
+		-- The regions have been defined.
+
+	-- If number of teams is any number other than two, use standard division.
+	else	
+		print("-"); print("Dividing the map at random."); print("-");
+		self.method = 2;	
+		local best_areas = {};
+		local globalFertilityOfLands = {};
+
+		-- Obtain info on all landmasses for comparision purposes.
+		local iGlobalFertilityOfLands = 0;
+		local iNumLandPlots = 0;
+		local iNumLandAreas = 0;
+		local land_area_IDs = {};
+		local land_area_plots = {};
+		local land_area_fert = {};
+		-- Cycle through all plots in the world, checking their Start Placement Fertility and AreaID.
+		for x = 0, iW - 1 do
+			for y = 0, iH - 1 do
+				local i = y * iW + x + 1;
+				local plot = Map.GetPlot(x, y);
+				if not plot:IsWater() then -- Land plot, process it.
+					iNumLandPlots = iNumLandPlots + 1;
+					local iArea = plot:GetArea();
+					local plotFertility = self:MeasureStartPlacementFertilityOfPlot(x, y, true); -- Check for coastal land is enabled.
+					iGlobalFertilityOfLands = iGlobalFertilityOfLands + plotFertility;
+					--
+					if TestMembership(land_area_IDs, iArea) == false then -- This plot is the first detected in its AreaID.
+						iNumLandAreas = iNumLandAreas + 1;
+						table.insert(land_area_IDs, iArea);
+						land_area_plots[iArea] = 1;
+						land_area_fert[iArea] = plotFertility;
+					else -- This AreaID already known.
+						land_area_plots[iArea] = land_area_plots[iArea] + 1;
+						land_area_fert[iArea] = land_area_fert[iArea] + plotFertility;
+					end
+				end
+			end
+		end
+		
+		-- Sort areas, achieving a list of AreaIDs with best areas first.
+		--
+		-- Fertility data in land_area_fert is stored with areaID index keys.
+		-- Need to generate a version of this table with indices of 1 to n, where n is number of land areas.
+		local interim_table = {};
+		for loop_index, data_entry in pairs(land_area_fert) do
+			table.insert(interim_table, data_entry);
+		end
+		-- Sort the fertility values stored in the interim table. Sort order in Lua is lowest to highest.
+		table.sort(interim_table);
+		-- If less players than landmasses, we will ignore the extra landmasses.
+		local iNumRelevantLandAreas = math.min(iNumLandAreas, self.iNumCivs);
+		-- Now re-match the AreaID numbers with their corresponding fertility values
+		-- by comparing the original fertility table with the sorted interim table.
+		-- During this comparison, best_areas will be constructed from sorted AreaIDs, richest stored first.
+		local best_areas = {};
+		-- Currently, the best yields are at the end of the interim table. We need to step backward from there.
+		local end_of_interim_table = table.maxn(interim_table);
+		-- We may not need all entries in the table. Process only iNumRelevantLandAreas worth of table entries.
+		for areaTestLoop = end_of_interim_table, (end_of_interim_table - iNumRelevantLandAreas + 1), -1 do
+			for loop_index, AreaID in ipairs(land_area_IDs) do
+				if interim_table[areaTestLoop] == land_area_fert[land_area_IDs[loop_index]] then
+					table.insert(best_areas, AreaID);
+					table.remove(land_area_IDs, landLoop);
+					break
+				end
+			end
+		end
+
+		-- Assign continents to receive start plots. Record number of civs assigned to each landmass.
+		local inhabitedAreaIDs = {};
+		local numberOfCivsPerArea = table.fill(0, iNumRelevantLandAreas); -- Indexed in synch with best_areas. Use same index to match values from each table.
+		for civToAssign = 1, self.iNumCivs do
+			local bestRemainingArea;
+			local bestRemainingFertility = 0;
+			local bestAreaTableIndex;
+			-- Loop through areas, find the one with the best remaining fertility (civs added 
+			-- to a landmass reduces its fertility rating for subsequent civs).
+			for area_loop, AreaID in ipairs(best_areas) do
+				local thisLandmassCurrentFertility = land_area_fert[AreaID] / (1 + numberOfCivsPerArea[area_loop]);
+				if thisLandmassCurrentFertility > bestRemainingFertility then
+					bestRemainingArea = AreaID;
+					bestRemainingFertility = thisLandmassCurrentFertility;
+					bestAreaTableIndex = area_loop;
+				end
+			end
+			-- Record results for this pass. (A landmass has been assigned to receive one more start point than it previously had).
+			numberOfCivsPerArea[bestAreaTableIndex] = numberOfCivsPerArea[bestAreaTableIndex] + 1;
+			if TestMembership(inhabitedAreaIDs, bestRemainingArea) == false then
+				table.insert(inhabitedAreaIDs, bestRemainingArea);
+			end
+		end
+				
+		-- Loop through the list of inhabited landmasses, dividing each landmass in to regions.
+		-- Note that it is OK to divide a continent with one civ on it: this will assign the whole
+		-- of the landmass to a single region, and is the easiest method of recording such a region.
+		local iNumInhabitedLandmasses = table.maxn(inhabitedAreaIDs);
+		for loop, currentLandmassID in ipairs(inhabitedAreaIDs) do
+			-- Obtain the boundaries of and data for this landmass.
+			local landmass_data = ObtainLandmassBoundaries(currentLandmassID);
+			local iWestX = landmass_data[1];
+			local iSouthY = landmass_data[2];
+			local iEastX = landmass_data[3];
+			local iNorthY = landmass_data[4];
+			local iWidth = landmass_data[5];
+			local iHeight = landmass_data[6];
+			local wrapsX = landmass_data[7];
+			local wrapsY = landmass_data[8];
+			-- Obtain "Start Placement Fertility" of the current landmass. (Necessary to do this
+			-- again because the fert_table can't be built prior to finding boundaries, and we had
+			-- to ID the proper landmasses via fertility to be able to figure out their boundaries.
+			local fert_table, fertCount, plotCount = self:MeasureStartPlacementFertilityOfLandmass(currentLandmassID, 
+		  	                                         iWestX, iEastX, iSouthY, iNorthY, wrapsX, wrapsY);
+			-- Assemble the rectangle data for this landmass.
+			local rect_table = {iWestX, iSouthY, iWidth, iHeight, currentLandmassID, fertCount, plotCount};
+			-- Divide this landmass in to number of regions equal to civs assigned here.
+			iNumCivsOnThisLandmass = numberOfCivsPerArea[loop];
+			if iNumCivsOnThisLandmass > 0 and iNumCivsOnThisLandmass <= 22 then -- valid number of civs.
+				self:DivideIntoRegions(iNumCivsOnThisLandmass, fert_table, rect_table)
+			else
+				print("Invalid number of civs assigned to a landmass: ", iNumCivsOnThisLandmass);
+			end
+		end
+	end
+end
+------------------------------------------------------------------------------
 function AssignStartingPlots:BalanceAndAssign()
 	-- This function determines what level of Bonus Resource support a location
 	-- may need, identifies compatibility with civ-specific biases, and places starts.
@@ -459,8 +683,8 @@ function StartPlotSystem()
 	-- tell the AI that we should treat this as a naval expansion map
 	Map.ChangeAIMapHint(1+4);
 	if (PreGame.IsMultiplayerGame()) then
-    	Network.SendChat("[COLOR_POSITIVE_TEXT]Lekmap v3.3[ENDCOLOR]", -1, -1);
-      Network.SendChat("[COLOR_POSITIVE_TEXT]Danny Map v1.0[ENDCOLOR]", -1, -1);
+          Network.SendChat("[COLOR_POSITIVE_TEXT]Lekmap v3.3[ENDCOLOR]", -1, -1);
+          Network.SendChat("[COLOR_POSITIVE_TEXT]Danny Map v1.0[ENDCOLOR]", -1, -1);
 	end
 end
 ------------------------------------------------------------------------------
